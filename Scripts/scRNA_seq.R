99caee4c0708a8dba4295edf88735eecbfa9319a
Bibhu Simkhada <bsimkha@g.clemson.edu>
2026-04-26 12:42:38 -0400

    Full workflow

--- Scripts/scRNA_seq.R
+++ Scripts/scRNA_seq.R
@@ -1,5 +1,6 @@
-#Load the Seurat library
+#Load libraries
 library(Seurat)
+library(dplyr)
 
 #Load the directory containing matrix files generated from CellRanger
 matrix_dir <- "~/Desktop/scRNA/1_Raw_data/filtered_feature_bc_matrix"
@@ -31,7 +32,7 @@ scatterplot
 
 #Use the violinplot to find the outliers and trim the data
 #Baseline gold standard is 200 < x < 2500
-scObject <- subset(scObject, nFeature_RNA>1000 & nFeature_RNA<3000 & percent.mt<10)
+scObject <- subset(scObject, nFeature_RNA>200 & nFeature_RNA<2500 & percent.mt<10)
 
 #Plot the figures again to make sure the trimming worked
 VlnPlot(scObject, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
@@ -41,7 +42,62 @@ plot2 <- FeatureScatter(scObject, feature1 = "nCount_RNA", feature2 = "nFeature_
 scatterplot <- plot1 + plot2
 scatterplot
 
-#Normalize the data
-Normalized_scObject <- NormalizeData(scObject)
+#Normalize the data using SCTransform
+Normalized_scObject <- SCTransform(scObject)
 head(Normalized_scObject)
 head(scObject)
+
+#Identify the highly variable genes across cell types
+VariableFeaturePlot(Normalized_scObject) #plot without the variable genes labeled
+top10 <- head(VariableFeatures(Normalized_scObject), 10) #Identify the top 10 variable genes
+LabelPoints(plot = VariableFeaturePlot(Normalized_scObject), points = top10, repel = TRUE) #Plot with the top genes labeled
+
+#Scale data with mean around 0
+Normalized_scObject <- ScaleData(Normalized_scObject)
+
+#Run the PCA
+Normalized_scObject <- RunPCA(Normalized_scObject)
+
+#Use elbow plot to identify the PC that don't add any further information
+ElbowPlot(Normalized_scObject)
+
+VizDimLoadings(Normalized_scObject, dims = 1:2)
+
+#Find clusters of "like"cell types
+Normalized_scObject <- FindNeighbors(Normalized_scObject, dims = 1:6) #the dimensions come from the eblow plot we plotted earlier
+Normalized_scObject <- FindClusters(Normalized_scObject, resolution = 0.5)
+
+#plot the UMAP to visaualize the data
+Normalized_scObject <- RunUMAP(Normalized_scObject, dims = 1:6)
+DimPlot(Normalized_scObject, reduction = "umap", label = TRUE)
+
+
+#Find marker genes for cell type annotation
+markers <- FindAllMarkers(Normalized_scObject, only.pos = TRUE)
+head(markers)
+
+#Find top markers
+top_markers <- markers %>%
+  group_by(cluster) %>%
+  slice_max(order_by = avg_log2FC, n = 5)
+
+top_markers #check this df to manually curate a cell-type annotation file
+#for example: Go to proteinatlas.org and search for the genes in the file
+#Choose single cell and identify cell type. Do this for all genes to identify consensus for each cluster
+
+#create annotation file
+annotation_df <- data.frame(
+  seurat_clusters = c("0","1","2","3","4"),
+  cell_type = c("T cells","B cells","Monocytes","NK cells","Neutrophil")
+)
+
+Normalized_scObject$cell_type <- annotation_df$cell_type[
+  match(as.character(Normalized_scObject$seurat_clusters),
+        annotation_df$seurat_clusters)]
+
+#Heat map for cell-type specific expression of top genes
+DoHeatmap(Normalized_scObject, features = head(markers$gene, 20))
+
+#Plot the UMAP with cell names labelled
+Idents(Normalized_scObject) <- Normalized_scObject$cell_type
+DimPlot(Normalized_scObject, reduction = "umap", repel = TRUE)
